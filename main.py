3998bc2e000ffac8ff921eeda485d9538298195b
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-13 21:38:42 +0000

    Deleted logger and hook.

--- main.py
+++ main.py
@@ -2,21 +2,6 @@ from __future__ import unicode_literals
 import yt_dlp
 
 
-class MyLogger(object):
-    def debug(self, msg):
-        pass
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        print(msg)
-
-
-def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
-
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -25,8 +10,6 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
 }
 
 def download(link):
