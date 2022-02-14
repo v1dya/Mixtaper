845d2e38363f46c51fb8a9242f6b964dfc41217f
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-14 00:43:03 +0000

    API completed.
    API's youtube endpoint returns an mp3 file with all the songs

---
+++ mixtaper.py
@@ -0,0 +1,48 @@
+import yt_dlp
+import glob
+from pydub import AudioSegment
+from pydub.utils import mediainfo
+import os
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+
+def download(link):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+
+
+def mix(links):
+    global bitrate, tag, mixtape
+    count = 0
+    for link in links:
+        download(link)
+        song_path = glob.glob("*.mp3")
+        if "mixtape.mp3" in song_path:
+            song_path.remove("mixtape.mp3")
+
+        song = AudioSegment.from_mp3(song_path[0])
+        if count == 0:
+            mixtape = song
+            count = count + 1
+        else:
+            mixtape = mixtape.append(song, crossfade=(10 * 1000))
+            count = count + 1
+
+        tag = mediainfo(song_path[0]).get('TAG', {})
+        bitrate = mediainfo(song_path[0])["bit_rate"]
+        os.remove(song_path[0])
+
+    mixtape = mixtape.fade_out(2000)
+
+    f = open("mixtape.mp3", 'wb')
+    mixtape.export(f, format="mp3", tags=tag, bitrate=bitrate)
+    print("Song exported as mixtape.mp3")
+    f.close()
