385d72286580714c01dbfc86affdd02d214e2f7d
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-03-01 02:25:23 +0000

    HTML and CSS responsive design completed.

--- Front-End/Mixtaper/mixtaper/src/Inputter.js
+++ Front-End/Mixtaper/mixtaper/src/Inputter.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/Inputter.css";
 
 function Inputter() {
+  const [links, setLinks] = useState("");
+  const [currLink, setcurrLink] = useState("");
+  const [linksJson, setlinksJson] = useState([]);
+
+  const addLink = (e) => {
+    e.preventDefault();
+    setLinks(links + currLink + "<br>");
+  };
+
+  useEffect(() => {
+    if(currLink!=""){
+      setlinksJson([...linksJson, currLink]);
+    }
+  }, [links]);
+
+  const mixtape = (e) =>{
+    e.preventDefault();
+    let jsonReq = {
+      "links":linksJson
+    }
+    
+  }
   return (
     <div className="inputter">
-      <div className="inputter__container">
-        <div className="inputter__mainBox">
-          <p className="inputter__inputLink">enter youtube links:</p>
-          <input className="inputter__text" type="text" />
-          <button className="inputter__addButton">add to mixtape</button>
-        </div>
+      <div className="inputter__inputText">
+        <h3>enter your youtube links:</h3>
       </div>
-      <div className="converter__container">
-        <div className="converter__button">mixtape</div>
+      
+        <form className="inputter__inputBox">
+        <input
+          className="inputBox__editText"
+          value={currLink}
+          placeholder="Enter links here"
+          onChange={(e) => setcurrLink(e.target.value)}
+        />
+        <button className="inputBox__addButton" onClick={addLink} type="submit">
+          <strong>add to mixtape</strong>
+        </button>
+        </form>
+      
+      <div className="inputter__mixtape">
+        <button className="mixtape__button" onClick={mixtape}>
+          <b>mixtape</b>
+        </button>
+      </div>
+      <div className="inputter__linkHistory">
+        <p>Links added:</p>
+        <p
+          className="inputter_links"
+          dangerouslySetInnerHTML={{ __html: links }}
+        ></p>
       </div>
     </div>
   );
