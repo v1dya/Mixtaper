16dcc2c8701b02b1558033b9888eeee16210c794
suryansh <55044178+Aydivv@users.noreply.github.com>
2022-03-04 09:20:59 +0000

    Added cors compatibility

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ['*']
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class Links(BaseModel):
